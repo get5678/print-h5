434aa85003c29785051d1f7b7e6d668ff148e01a
pangonghang <1966656576@qq.com>
2019-07-16 20:08:54 +0800

    clear

--- src/utils/api.ts
+++
@@ -1,9 +0,0 @@
-import Http from './http';
-
-const http = new Http();
-
-const baseUrl = 'https://';
-
-export async function test(data, contentType?) {
-  return await http.get(`${baseUrl}/api/test`, data, contentType);
-}
