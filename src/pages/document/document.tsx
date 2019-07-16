79c4d23f765b5237704f65278c79dce4bb497084
longrou <909593143@qq.com>
2019-07-16 20:12:08 +0800

    document

--- src/pages/document/document.tsx
+++ src/pages/document/document.tsx
@@ -156,24 +156,6 @@ class Document extends Component<IProps, PageState> {
                 console.log(data,"data")
             }
         })
-
-        // Taro.chooseImage({
-        //     success: function (res) {
-        //         var tempFilePaths = res.tempFilePaths
-        //         Taro.uploadFile({
-        //             url: 'https://example.weixin.qq.com/upload', //仅为示例，非真实的接口地址
-        //             filePath: tempFilePaths[0],
-        //             name: 'file',
-        //             formData: {
-        //                 'user': 'test'
-        //             },
-        //             success: function (res) {
-        //                 var data = res.data
-        //                 //do something
-        //             }
-        //         })
-        //     }
-        // })
     }
 
     handleChange = e => {
