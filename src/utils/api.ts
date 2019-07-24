ccd80b8a8fde631ac3ec5480d9cdc3513c3803b6
pangonghang <1966656576@qq.com>
2019-07-24 11:38:20 +0800

    更新域名

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -2,24 +2,24 @@ import Http from './http';
 
 const http = new Http();
 
-const baseUrl = 'https://cloudprint.our16.top/cloudprint';
+const baseUrl = 'https://pin.varbee.com/cloudprint/api/';
 // 店铺详情接口
 export async function getShopDetail(data, contentType?) {
-  return await http.get(`${baseUrl}/api/merchant/getShopDetail`, data, contentType);
+  return await http.get(`${baseUrl}merchant/getShopDetail`, data, contentType);
 }
 // 订单详情接口
 export async function orderDetail(data, contentType?) {
-  return await http.get(`${baseUrl}/api/order/orderDetail`, data, contentType);
+  return await http.get(`${baseUrl}order/orderDetail`, data, contentType);
 }
 //正在打印的订单列表接口
 export async function printingList(data, contentType?) {
-  return await http.post(`${baseUrl}/api/order/printingList`, data, contentType);
+  return await http.post(`${baseUrl}order/printingList`, data, contentType);
 }
 // 反馈页面接口
 export async function feedBack(data, contentType?) {
-  return await http.post(`${baseUrl}/api/feedback/feedback`, data, contentType);
+  return await http.post(`${baseUrl}feedback/feedback`, data, contentType);
 }
 // 我的订单页面接口
 export async function myOrderList(data, contentType?) {
-  return await http.post(`${baseUrl}/api/order/myOrderList`, data, contentType);
+  return await http.post(`${baseUrl}order/myOrderList`, data, contentType);
 }
